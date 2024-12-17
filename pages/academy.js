731fe011ee998e6fc3b64492b4c42e08a23ea7e0
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 02:33:11 +0200

    Create academy.js

---
+++ pages/academy.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Academy() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>TechBridge Academy</h1>
+        <p>Coming Soon!</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
