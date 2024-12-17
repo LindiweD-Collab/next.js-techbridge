7746ed15ddae849ec6979c3cdb16b4b045185ea6
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 02:29:13 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Header />
+      <main>
+        <section className="hero" style={{ backgroundImage: `url(/bg-image.jpg)` }}>
+          <h1>Welcome to TechBridge Solutions</h1>
+          <p>Innovative Software Solutions for Your Business Needs</p>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+      
