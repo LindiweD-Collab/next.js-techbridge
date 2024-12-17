5704ca6dc9468df8a4ad621ac5bb6af825409336
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 02:04:25 +0200

    Create Services.js

---
+++ app/Services.js
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Services() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Our Services</h1>
+        <ul>
+          <li>Software Development Solutions</li>
+          <li>Business Digitization</li>
+          <li>SEO Services</li>
+          <li>RPA Solutions</li>
+        </ul>
+      </main>
+      <Footer />
+    </div>
+  );
+}
