38ec479e0602eedbdd87baa3aec4943c2becb1f3
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 02:35:02 +0200

    Create webinar.js

---
+++ pages/webinar.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Webinars() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Webinars and Resources</h1>
+        <p>Check out our latest webinars and resources.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
