faa4d4cae35bb44fad89c16a639aa2d27e58985c
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 09:24:22 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,22 @@
-const Footer = () => (
-  <footer className="footer">
-    <p>© 2024 TechBridge Solutions. All rights reserved.</p>
-  </footer>
-);
+Here's an example of how you can style the `Footer` component using TypeScript and make it look appealing:
+
+```
+import React from 'react';
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = () => {
+  return (
+    <footer className="footer bg-aquamarine-900 text-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <p className="text-lg text-center">
+          &copy; 2024 TechBridge Solutions. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
+
+
