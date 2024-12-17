5cae28ae183d521624a173d0e27794b036ad5449
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 02:37:29 +0200

    Create blogs.js

---
+++ pages/blogs.js
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Webinars(
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Blogs() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Our Blog</h1>
+        <p>Stay updated with the latest insights and updates.</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
