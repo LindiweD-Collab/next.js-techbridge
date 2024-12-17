4e093775069a09577136d6933e6f21ab69c8edd9
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-12-17 09:37:20 +0200

    Update academy.js

--- pages/academy.js
+++ pages/academy.js
@@ -1,15 +1,27 @@
+import React from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function Academy() {
+interface AcademyProps {}
+
+const Academy: React.FC<AcademyProps> = () => {
   return (
-    <div>
+    <div className="bg-white h-screen">
       <Header />
-      <main>
-        <h1>TechBridge Academy</h1>
-        <p>Coming Soon!</p>
+      <main className="container mx-auto p-4 pt-6 md:p-6 lg:p-8 xl:p-10">
+        <h1 className="text-purple-700 text-4xl font-bold mb-4">
+          TechBridge Academy
+        </h1>
+        <p className="text-purple-700 text-lg mb-8">
+          Coming Soon!
+        </p>
+        <button className="bg-lime-500 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
+          Learn More
+        </button>
       </main>
       <Footer />
     </div>
   );
-}
+};
+
+export default Academy;
